fed0ea033faa583e7215630a3ab64b6361638406
Dinar Gimazetdinov <gdinar92@gmail.com>
2017-01-18 10:15:45 +0300

    Edited method echo() for testing

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,11 +1,11 @@
 package ru.job4j;
 
 public class Calculate {
-	public void echo() {
-		System.out.println("Hello world!");
+	public String echo(String str) {
+		return String.format("%s", str);
 	}
 	public static void main(String[] args) {
 		Calculate calc = new Calculate();
-		calc.echo();
+		System.out.println(calc.echo("Hello world!"));
 	}
 }
