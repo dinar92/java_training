2b0650b6164e36e3fe134002169bba79e43468c2
Dinar Gimazetdinov <gdinar92@gmail.com>
2017-01-24 10:15:07 +0300

    Chapter 3.2 Added Max class

---
+++ chapter_003/src/main/java/ru/job4j/Max.java
@@ -0,0 +1,19 @@
+package ru.job4j;
+
+/**Class for determining max value.
+*@author gimazetdinov
+*@version 1.0
+*@since 24.01.2017
+*/
+public class Max {
+
+	/**
+	*Return max of two params.
+	*@param first - first arg
+	*@param second - second arg
+	*@return int value
+	*/
+	public int max(int first, int second) {
+		return first > second ? first : second;
+	}
+}
