920f210636252180f15e3aad3e023a2aa1c6f003
Dinar Gimazetdinov <gdinar92@gmail.com>
2017-01-24 20:19:25 +0300

    Chapter 4.3. Class to paint a pyramids

---
+++ chapter_004/src/main/java/ru/job4j/Paint.java
@@ -0,0 +1,27 @@
+package ru.job4j;
+
+/**Class for paint pyramids.
+*@author gimazetdinov
+*@version 1.0
+*@since 24.01.2017
+*/
+public class Paint {
+
+	/**Method which paint a pyramid.
+	*@param h - height of a pyramid
+	*@return paint of pyramid in the String format
+	*/
+	public String pyramid(int h) {
+		StringBuilder str = new StringBuilder();
+		for (int i = 0; i < h; i++) {
+			for (int j = i; j < h - 1; j++) {
+				str.append(" ");
+			}
+			for (int j = 0; j < i + 1; j++) {
+				str.append(" ^");
+			}
+			str.append("\n");
+		}
+		return str.toString();
+	}
+}
