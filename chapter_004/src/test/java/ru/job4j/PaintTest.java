8461192755a10684efd7f257af6a7aae0215090a
Dinar Gimazetdinov <gdinar92@gmail.com>
2017-01-24 20:20:33 +0300

    Chapter 4.3. Added test for class which paint pyramids

---
+++ chapter_004/src/test/java/ru/job4j/PaintTest.java
@@ -0,0 +1,21 @@
+package ru.job4j;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**Testing the class for paint pyramids.
+*@author gimazetdinov
+*@version 1.0
+*@since 24.01.2017
+*/
+public class PaintTest {
+
+	/**Testing pyramid().*/
+	@Test
+	public void whenHeightIsTwoThenPyramid() {
+		final int height = 2;
+		Paint paint = new Paint();
+		assertThat(paint.pyramid(height), is("  ^\n ^ ^\n"));
+	}
+}
