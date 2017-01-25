8bbb25cb2fe9aab88ecaf55ea685410d26a400b1
Dinar Gimazetdinov <gdinar92@gmail.com>
2017-01-25 14:27:05 +0300

    Chapter 5.2. Tests for rotated array

--- chapter_001/src/test/java/ru/job4j/array/TurnTest.java
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -10,12 +10,24 @@ import static org.junit.Assert.assertThat;
 *@since 24.01.2017
 */
 public class TurnTest {
-	/**Testing bec().*/
+	/**Testing back().*/
 	@Test
 	public void whenArrayThenRevertedArray() {
 		final int[] arr = {6, 5, 4, 3, 2, 1};
 		final int[] revertedArr = {1, 2, 3, 4, 5, 6};
 		Turn obj = new Turn();
 		assertThat(obj.back(arr), is(revertedArr));
+	}
+
+	/**Testing rotate().*/
+	public void whenArrayThenRotatedArray() {
+		final int[][] arr = {{1, 2, 3},
+							 {4, 5, 6},
+							 {7, 8, 9}};
+		final int[][] rotatedArr = {{7, 4, 1},
+									{8, 5, 2},
+									{9, 6, 3}};
+		Turn obj = new Turn();
+		assertThat(obj.rotate(arr), is(rotatedArr));
 	}
 }
