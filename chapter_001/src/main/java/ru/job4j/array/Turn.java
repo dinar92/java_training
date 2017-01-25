a276f1ba963d0f930d06d46df4b3a611c9bf9483
Dinar Gimazetdinov <gdinar92@gmail.com>
2017-01-25 14:26:08 +0300

    Chapter 5.2. Rotate array

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -20,4 +20,23 @@ public class Turn {
 		}
 		return inputArray;
 	}
+
+	/**Method which rotate the square array.
+	*@param inputArray unrotated array
+	*@return inputArray rotated array
+	*/
+	public int[][] rotate(int[][] inputArray) {
+		int tmp = 0;
+		int len = inputArray.length;
+		for (int i = 0; i < len / 2; i++) {
+			for (int j = i; j < len - 1 - i; j++) {
+				tmp = inputArray[i][j];
+				inputArray[i][j] = inputArray[len - j - 1][i];
+				inputArray[len - j - 1][i] = inputArray[len - i - 1][len - j - 1];
+				inputArray[len - i - 1][len - j - 1] = inputArray[j][len - i - 1];
+				inputArray[j][len - i - 1] = tmp;
+			}
+		}
+		return inputArray;
+	}
 }
