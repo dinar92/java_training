b105428f12572392e489bc64f6748c34ee930c82
Dinar Gimazetdinov <gdinar92@gmail.com>
2017-03-10 13:47:00 +0300

    Chapter 3.1.1 Tests for isEvenNumber()

---
+++ chapter_003/src/test/java/ru/job4j/ioservice/ServiceTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.ioservice;
+
+import org.junit.Test;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**Tests Service.*/
+public class ServiceTest {
+
+    /**Tests isEvenNumber().
+     * @throws IOException IOException*/
+    @Test
+    public void whenIsEvenNumberThenTrue() throws IOException {
+        Service service = new Service();
+        byte[] arr = {2};
+        try (ByteArrayInputStream inputStream = new ByteArrayInputStream(arr)) {
+            assertThat(service.isEvenNumber(inputStream), is(true));
+        }
+    }
+
+    /**Tests isEvenNumber().
+     * @throws IOException IOException*/
+    @Test
+    public void whenIsNotEvenNumberThenFalse() throws IOException {
+        Service service = new Service();
+        byte[] arr = {1};
+        try (ByteArrayInputStream inputStream = new ByteArrayInputStream(arr)) {
+            assertThat(service.isEvenNumber(inputStream), is(false));
+        }
+    }
+}
