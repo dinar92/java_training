2ee5b3bfad52c6832fb336829bc2427aa3a510df
Dinar Gimazetdinov <gdinar92@gmail.com>
2017-03-15 00:49:55 +0300

    Chapter 3.1.2 A filter for an input stream

--- chapter_003/src/main/java/ru/job4j/ioservice/Service.java
+++ chapter_003/src/main/java/ru/job4j/ioservice/Service.java
@@ -2,18 +2,64 @@ package ru.job4j.ioservice;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 
 /**IO handling.*/
 public class Service {
 
-    /**Parity checks InputStream's numbers.
+    /**
+     * Parity checks InputStream's numbers.
+     *
      * @param in - input stream
      * @return number is even
-     * @throws  IOException IOException
+     * @throws IOException IOException
      */
     public boolean isEvenNumber(InputStream in) throws IOException {
         return in.read() % 2 != 1;
     }
 
+    /**Filters out forbidden words.
+     * @param in - input stream
+     * @param out - output stream
+     * @param abuse - forbidden words
+     * @throws IOException IOException
+     */
+    public void dropAbuses(InputStream in, OutputStream out, String[] abuse) throws IOException {
+        try (InputStreamReader charIn = new InputStreamReader(in); OutputStreamWriter charOut = new OutputStreamWriter(out)) {
+            int currentChar = charIn.read();
+            int longestWordsLength = 0;
+
+            for (String word : abuse) {
+                if (word.length() > longestWordsLength) {
+                    longestWordsLength = word.length();
+                }
+            }
+
+            int[][] queueCollector = new int[abuse.length + 1][longestWordsLength];
+
+            while (currentChar != -1) {
+                boolean mustOut = true;
+                for (int wordCounter = 0; wordCounter < abuse.length; wordCounter++) {
+                    System.out.println(queueCollector[abuse.length][wordCounter]);
+                    if (currentChar == abuse[wordCounter].charAt(queueCollector[abuse.length][wordCounter])) {
+                        queueCollector[wordCounter][queueCollector[abuse.length][wordCounter]] = currentChar;
+                        queueCollector[abuse.length][wordCounter]++;
+                        mustOut = false;
+                    } else {
+                        queueCollector[abuse.length][wordCounter] = 0;
+                    }
+                    if (queueCollector[abuse.length][wordCounter] == abuse[wordCounter].length()) {
+                        queueCollector[abuse.length][wordCounter] = 0;
+                    }
+                }
+                if (mustOut) {
+                    charOut.write(currentChar);
+                }
+                currentChar = charIn.read();
+            }
+        }
+    }
 }
--- chapter_003/src/test/java/ru/job4j/ioservice/ServiceTest.java
+++ chapter_003/src/test/java/ru/job4j/ioservice/ServiceTest.java
@@ -2,7 +2,9 @@ package ru.job4j.ioservice;
 
 import org.junit.Test;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -10,11 +12,13 @@ import static org.junit.Assert.assertThat;
 /**Tests Service.*/
 public class ServiceTest {
 
+    /**An object of a service for tests.*/
+    Service service = new Service();
+
     /**Tests isEvenNumber().
      * @throws IOException IOException*/
     @Test
     public void whenIsEvenNumberThenTrue() throws IOException {
-        Service service = new Service();
         byte[] arr = {2};
         try (ByteArrayInputStream inputStream = new ByteArrayInputStream(arr)) {
             assertThat(service.isEvenNumber(inputStream), is(true));
@@ -25,10 +29,24 @@ public class ServiceTest {
      * @throws IOException IOException*/
     @Test
     public void whenIsNotEvenNumberThenFalse() throws IOException {
-        Service service = new Service();
         byte[] arr = {1};
         try (ByteArrayInputStream inputStream = new ByteArrayInputStream(arr)) {
             assertThat(service.isEvenNumber(inputStream), is(false));
+        }
+    }
+
+    /**Tests dropAbuse().
+     * @throws IOException IOException*/
+    @Test
+    public void whenInputThenFilteredOutput() throws IOException {
+        byte[] inputStream = new String("streamToFilter".toCharArray()).getBytes(StandardCharsets.UTF_8);
+        byte[] expectOutStream = new String("stToter".toCharArray()).getBytes(StandardCharsets.UTF_8);
+        String[] abuseWords = {"Fil", "ream"};
+        try (ByteArrayInputStream input = new ByteArrayInputStream(inputStream); ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            service.dropAbuses(input, out, abuseWords);
+            for (int i = 0; i < out.toByteArray().length; i++) {
+                assertThat(out.toByteArray()[i], is(expectOutStream[i]));
+            }
         }
     }
 }
