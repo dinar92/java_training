d7876f85773992058cd941c01cb0dc578c728d34
Dinar Gimazetdinov <gdinar92@gmail.com>
2017-03-23 23:40:44 +0300

    Chapter 3.1.3 Large files sort with external files

--- chapter_003/src/main/java/ru/job4j/ioservice/Service.java
+++ chapter_003/src/main/java/ru/job4j/ioservice/Service.java
@@ -111,28 +111,44 @@ public class Service {
 //
 //    }
 
-    /**The method for sorting large text files by length of strings.
+    /**
+     * The method for sorting large text files by length of strings.
      * Is intended for max size of RAM 512Mb.
      * Creates new file "destination.data"
+     *
      * @param source - a source file
      * @throws IOException IOException
-     * */
+     */
     public void textFileSort(File source) throws IOException {
-        //Хранилище для подмассивов.
-        List<List> tmpArrays = new ArrayList<>();
+        //Хранилище для разбитых файлов.
+        List<File> tmpArrays = new ArrayList<>();
+        int filesCounter = 0;
         try (RandomAccessFile sourceFile = new RandomAccessFile(source, "r")) {
 
-            //Разбиваем файл на подмассивы строк по 500Мб.
+            //Разбиваем файл на файлы с подмассивами строк по 500Мб.
             while (sourceFile.getFilePointer() < sourceFile.length() - 1) {
-                List<String> tmpArray = new ArrayList<>();
+                File tmpFile = new File(String.format("%s.data", filesCounter++));
+                tmpFile.createNewFile();
                 int lineBytesCounter = 0;
-                while (lineBytesCounter  < 500000000 && sourceFile.getFilePointer() < sourceFile.length() - 1) {
-                    String tmpLine = sourceFile.readLine();
-                    lineBytesCounter += tmpLine.getBytes().length;
-                    tmpArray.add(tmpLine + "\n");
+                try (RandomAccessFile raf = new RandomAccessFile(tmpFile, "rw")) {
+                    while (lineBytesCounter < 500000000 && sourceFile.getFilePointer() < sourceFile.length() - 1) {
+                        String tmpLine = sourceFile.readLine();
+                        lineBytesCounter += tmpLine.getBytes().length;
+                        raf.writeBytes(tmpLine + "\n");
+                    }
                 }
+                tmpArrays.add(tmpFile);
+            }
+        }
 
-                //Сортировка подмассива методом вставки в порядке возрастания.
+        //Сортировка каждого файла методом вставки в порядке возрастания и его перезапись.
+        for (File tmpFile : tmpArrays) {
+            try (RandomAccessFile raf = new RandomAccessFile(tmpFile, "rw")) {
+                List<String> tmpArray = new ArrayList<>();
+                while (raf.getFilePointer() < raf.length() - 1) {
+                    tmpArray.add(raf.readLine());
+                }
+                tmpFile.delete();
                 int out, in;
                 for (out = 1; out < tmpArray.size(); out++) {
                     String tmp = tmpArray.get(out);
@@ -143,36 +159,50 @@ public class Service {
                     }
                     tmpArray.set(in, tmp);
                 }
-                tmpArrays.add(tmpArray);
-            }
-        }
-        //Создаем массив для хранения всех строк и вставляем первый подмассив.
-        List<String> sortedArr = new ArrayList<>();
-        if (sortedArr.size() == 0) {
-            for (int i = 0; i < tmpArrays.get(0).size(); i++) {
-                sortedArr.add((String) tmpArrays.get(0).get(i));
+                tmpFile.createNewFile();
+                for (String str : tmpArray) {
+                    raf.writeBytes(str + "\n");
+                }
             }
         }
 
-        //Слияние упорядоченных подмассивов.
-        for (int out = 1; out < tmpArrays.size(); out++) {
-            int i = 0, j = 0;
-            while (i < sortedArr.size() && j < tmpArrays.get(out).size()) {
-                if (sortedArr.get(i).compareTo((String) tmpArrays.get(out).get(j)) > 0) {
-                    sortedArr.add(i, (String) tmpArrays.get(out).get(j));
-                    j++;
-                } else {
-                    i++;
-                }
+
+        //Создаем массив для хранения всех строк и вставляем первый файл и удаляем его.
+        List<String> sortedArr = new ArrayList<>();
+        try (RandomAccessFile raf = new RandomAccessFile(tmpArrays.get(0), "r")) {
+            while (raf.getFilePointer() < raf.length() - 1) {
+                sortedArr.add(raf.readLine());
             }
-            //Вставляем остатки от вставляемого подмассива в конец отсортированного,
-            //если закончился раньше.
-            if (i == sortedArr.size()) {
-                while (j < tmpArrays.get(out).size()) {
-                    sortedArr.add(i, (String) tmpArrays.get(out).get(j++));
-                }
-            }
+        } finally {
+            tmpArrays.get(0).delete();
         }
+
+
+        //Слияние упорядоченных подмассивов и удаление временных файлов.
+        for (int fileCounter = 1; fileCounter < tmpArrays.size(); fileCounter++) {
+            try (RandomAccessFile raf = new RandomAccessFile(tmpArrays.get(fileCounter), "r")) {
+                List<String> tmpArray = new ArrayList<>();
+                while (raf.getFilePointer() < raf.length() - 1) {
+                    tmpArray.add(raf.readLine());
+                }
+                int i = 0, j = 0;
+                while (i < sortedArr.size() && j < tmpArray.size()) {
+                    if (sortedArr.get(i).compareTo(tmpArray.get(j)) > 0) {
+                        sortedArr.add(i, tmpArray.get(j));
+                        j++;
+                    } else {
+                        i++;
+                    }
+                }
+                if (i == sortedArr.size()) {
+                    while (j < tmpArray.size()) {
+                        sortedArr.add(i, tmpArray.get(j++));
+                    }
+                }
+            }
+            tmpArrays.get(fileCounter).delete();
+        }
+
         //Создание файла и запись строк.
         File destination = new File("destination.data");
         destination.createNewFile();
