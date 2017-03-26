ad54c0c167871df8951d8680636c74c4fbc3b283
Dinar Gimazetdinov <gdinar92@gmail.com>
2017-03-26 22:20:27 +0300

    Chapter 3.1.2 With using BufferedReader

--- chapter_003/src/main/java/ru/job4j/ioservice/Service.java
+++ chapter_003/src/main/java/ru/job4j/ioservice/Service.java
@@ -1,10 +1,6 @@
 package ru.job4j.ioservice;
 
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.RandomAccessFile;
-import java.io.IOException;
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,82 +30,17 @@ public class Service {
      * @throws IOException IOException
      */
     public void dropAbuses(InputStream charIn, OutputStream charOut, String[] abuse) throws IOException {
-        int currentChar = charIn.read();
-        int longestWordsLength = 0;
-
-        for (String word : abuse) {
-            if (word.length() > longestWordsLength) {
-                longestWordsLength = word.length();
-            }
-        }
-
-        int[][] queueCollector = new int[abuse.length + 1][longestWordsLength];
-
-        while (currentChar != -1) {
-            boolean mustOut = true;
-            for (int wordCounter = 0; wordCounter < abuse.length; wordCounter++) {
-                System.out.println(queueCollector[abuse.length][wordCounter]);
-                if (currentChar == abuse[wordCounter].charAt(queueCollector[abuse.length][wordCounter])) {
-                    queueCollector[wordCounter][queueCollector[abuse.length][wordCounter]] = currentChar;
-                    queueCollector[abuse.length][wordCounter]++;
-                    mustOut = false;
-                } else {
-                    queueCollector[abuse.length][wordCounter] = 0;
-                }
-                if (queueCollector[abuse.length][wordCounter] == abuse[wordCounter].length()) {
-                    queueCollector[abuse.length][wordCounter] = 0;
+        try (BufferedReader charInBuffRead = new BufferedReader(new InputStreamReader(charIn));
+        BufferedWriter charOutBuffWriter = new BufferedWriter(new OutputStreamWriter(charOut))) {
+            String str = charInBuffRead.readLine();
+            for (String substr : abuse) {
+                if (str.contains(substr)) {
+                    str = str.replace(substr, "");
                 }
             }
-            if (mustOut) {
-                charOut.write(currentChar);
-            }
-            currentChar = charIn.read();
+            charOutBuffWriter.write(str);
         }
     }
-
-//    public void sort(File source, File destination) throws IOException {
-//        int filesCounter = 0;
-//        int amountLines = 0;
-//        try (RandomAccessFile file = new RandomAccessFile(source, "r")) {
-//            while (file.getFilePointer() < file.length()) {
-//                int lineCounter = 0;
-//                new File(String.format("%s.data", filesCounter));
-//                try (RandomAccessFile tempFile = new RandomAccessFile(String.format("%s.data", filesCounter), "rw")) {
-//                    while (tempFile.length() < 512000000 || tempFile.getFilePointer() != -1) {
-//                        tempFile.writeUTF(file.readLine().ge);
-//                        lineCounter++;
-//                    }
-//                    amountLines += lineCounter;
-//                    String[] arrOfStrings = new String[lineCounter];
-//                    tempFile.seek(0L);
-//                    for (int counter = 0; counter < arrOfStrings.length; counter++) {
-//                        arrOfStrings[counter] = tempFile.readLine();
-//                    }
-//                    for (int outer = 0; outer < arrOfStrings.length; outer++) {
-//                        for (int inner = 0; inner < arrOfStrings.length - outer - 1; inner++) {
-//                            if (arrOfStrings[inner].length() > arrOfStrings[inner + 1].length()) {
-//                                String tmp = arrOfStrings[inner + 1];
-//                                arrOfStrings[inner + 1] = arrOfStrings[inner];
-//                                arrOfStrings[inner] = tmp;
-//                            }
-//                        }
-//                    }
-//                    tempFile.seek(0L);
-//                    for (int counter = 0; counter < arrOfStrings.length; counter++) {
-//                        tempFile.writeUTF(arrOfStrings[counter]);
-//                    }
-//                }
-//                filesCounter++;
-//            }
-//        }
-//        String[] destArr = new String[amountLines];
-//        for (int i = 0; i < filesCounter - 1; i++) {
-//
-//        }
-//        source = new File("destination.data", "rw");
-//
-//
-//    }
 
     /**
      * The method for sorting large text files by length of strings.
