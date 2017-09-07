d04c7cd3370522ecb177d8b04a944c0ef12a19a1
Dinar Gimazetdinov <gdinar92@gmail.com>
2017-09-08 00:28:16 +0300

    Added chapter 5

---
+++ chapter_005/src/main/java/ru/job4j/collectionsPro/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Created by pacman on 06.09.17.
+ * Collections tutorials.
+ */
+package ru.job4j.collectionsPro;
---
+++ chapter_005/src/test/java/ru/job4j/collectionsPro/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Created by pacman on 06.09.17.
+ *
+ * Tests package collectionsPro.
+ */
+package ru.job4j.collectionsPro;
