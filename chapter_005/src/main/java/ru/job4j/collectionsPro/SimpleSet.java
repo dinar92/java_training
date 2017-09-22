4d499abe2f0040b7ee28bf21345bcd16212098ea
Dinar Gimazetdinov <gdinar92@gmail.com>
2017-09-22 21:40:39 +0300

    Chapter 5.4.1 The array set

---
+++ chapter_005/src/main/java/ru/job4j/collectionsPro/SimpleSet.java
@@ -0,0 +1,108 @@
+package ru.job4j.collectionsPro;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Created by pacman on 21.09.17.
+ * The simple Set implementation.
+ *
+ * @param <E> - an universal type.
+ */
+public class SimpleSet<E> implements Iterable {
+
+    /**
+     * A store.
+     */
+    private Object[] store = new Object[100];
+
+    /**
+     * An index of the last empty position in the store.
+     */
+    private int index = 0;
+
+    /**
+     * Adds element to the store.
+     *
+     * @param element - an element.
+     */
+    public void add(E element) {
+        if (!contains(element)) {
+            store[index++] = element;
+        }
+    }
+
+    /**
+     * Checks, that the element already exists in the array.
+     *
+     * @param element - the element.
+     * @return true - if exist, false - not exist.
+     */
+    public boolean contains(E element) {
+        boolean result = false;
+        for (Object object : store) {
+            if (element.equals(object)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+
+    /**
+     * Returns an instance of the iterator of this set.
+     *
+     * @return - an iterator.
+     */
+    public Iterator<E> iterator() {
+        return new SimpleSetIterator<E>();
+    }
+
+    /**
+     * The iterator of the Set.
+     *
+     * @param <E> - an universal type.
+     */
+    private class SimpleSetIterator<E> implements Iterator<E> {
+
+        /**
+         * An index of the last empty position of the iterator.
+         */
+        private int index = 0;
+
+        /**
+         * Returns {@code true} if the iteration has more elements.
+         * (In other words, returns {@code true} if {@link #next} would
+         * return an element rather than throwing an exception.)
+         *
+         * @return {@code true} if the iteration has more elements
+         */
+        @Override
+        public boolean hasNext() {
+            return SimpleSet.this.store[this.index] != null;
+        }
+
+        /**
+         * Returns the next element in the iteration.
+         *
+         * @return the next element in the iteration
+         * @throws NoSuchElementException if the iteration has no more elements
+         */
+        @Override
+        public E next() {
+            E result;
+            try {
+                result = (E) SimpleSet.this.store[this.index];
+                if (result == null) {
+                    throw new NoSuchElementException();
+                } else {
+                    this.index++;
+                }
+            } catch (ArrayIndexOutOfBoundsException ex) {
+                throw new NoSuchElementException();
+            }
+            return result;
+        }
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/collectionsPro/SimpleSetTest.java
@@ -0,0 +1,100 @@
+package ru.job4j.collectionsPro;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+/**
+ * Created by pacman on 21.09.17.
+ * Tests the class SimpleSet.
+ */
+public class SimpleSetTest {
+
+    /**
+     * Tests add().
+     */
+    @Test
+    public void whenAddNewElementThenItInSet() {
+        String element = "test1";
+        SimpleSet<String> set = new SimpleSet<>();
+
+        set.add(element);
+
+        assertThat(set.contains(element), is(true));
+    }
+
+    /**
+     * Tests add(), when adding the same elements.
+     */
+    @Test
+    public void whenAddingTheSameThenItNotAdded() {
+        String element = "test1";
+        String element2 = "test1";
+        SimpleSet<String> set = new SimpleSet<>();
+        Iterator<String> iterator = set.iterator();
+        set.add(element);
+        set.add(element2);
+
+        //contains only one element.
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(element));
+        assertThat(iterator.hasNext(), is(false));
+    }
+
+    /**
+     * Tests contains(), when element not exist in the set.
+     */
+    @Test
+    public void whenElementNotInSetThenNotContains() {
+        String element = "test1";
+        String element2 = "Not exist";
+        SimpleSet<String> set = new SimpleSet<>();
+
+        set.add(element);
+
+        assertThat(set.contains(element2), is(false));
+    }
+
+    /**
+     * Tests next(), when set not contains any element.
+     */
+    @Test(expected = NoSuchElementException.class)
+    public void whenElementNotExistInSetThenException() {
+        String element = "test1";
+        SimpleSet<String> set = new SimpleSet<>();
+
+        set.add(element);
+        Iterator<String> iterator = set.iterator();
+
+        iterator.next();
+        iterator.next();
+    }
+
+    /**
+     * Tests iterator.next().
+     */
+    @Test
+    public void whenHasNextThenGetNextElementFromSet() {
+        String element = "test1";
+        String element2 = "test2";
+        String element3 = "test3";
+        SimpleSet<String> set = new SimpleSet<>();
+        set.add(element);
+        set.add(element2);
+        set.add(element3);
+
+        Iterator<String> iterator = set.iterator();
+
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(element));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(element2));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(element3));
+    }
+}
