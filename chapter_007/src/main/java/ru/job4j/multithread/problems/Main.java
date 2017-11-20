5fee5bd20e4668ee5d8c1de3be7c3a348faf053a
dinar92 <gdinar92@gmail.com>
2017-11-20 17:47:17 +0300

    Muck

--- chapter_007/src/main/java/ru/job4j/multithread/problems/Main.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.multithread.problems;
-
-public class Main {
-
-    public static void main(String[] args) {
-        new VisibilityRunner().run();
-    }
-}
---
+++ chapter_007/src/test/java/ru/job4j/multithread/problems/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Created by user on 20.11.17.
+ * Tests multithreading troubles.
+ */
+package ru.job4j.multithread.problems;
